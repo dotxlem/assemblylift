bdb9fcf7d5f3229b5e92cc0adbd369a45950605c
xlem <xlem@akkoro.io>
2022-09-27 12:44:18 -0230

    refactor term mod; add service list
    the list isnt nested correctly yet

---
+++ cli/src/commands/term/app.rs
@@ -0,0 +1,60 @@
+use tui::widgets::ListState;
+
+pub struct App<'a> {
+    pub(crate) services: StatefulList<(&'a str, usize)>,
+}
+
+impl<'a> App<'a> {
+    pub(crate) fn new() -> App<'a> {
+        App {
+            services: Default::default(),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct StatefulList<T> {
+    pub(crate) state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
--- cli/src/commands/term/mod.rs
+++ cli/src/commands/term/mod.rs
@@ -1,18 +1,24 @@
+mod app;
+
 use std::io;
 use std::io::Stdout;
 use std::rc::Rc;
 
-use clap::{ArgMatches, crate_version};
-use crossterm::{event, execute};
+use clap::{crate_version, ArgMatches};
 use crossterm::event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
-use tui::{Frame, Terminal};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use crossterm::{event, execute};
+use itertools::Itertools;
 use tui::backend::CrosstermBackend;
 use tui::layout::{Alignment, Constraint, Direction, Layout};
 use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, Paragraph};
+use tui::widgets::{Block, Borders, List, ListItem, Paragraph};
+use tui::{Frame, Terminal};
 
 use crate::projectfs::Project;
+use crate::term::app::App;
 use crate::transpiler::toml::asml::Manifest;
 
 pub fn command(_matches: Option<&ArgMatches>) {
@@ -22,16 +28,22 @@ pub fn command(_matches: Option<&ArgMatches>) {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
-    let res = run_term(&mut terminal);
+    let mut app = App::new();
+    let res = run_term(&mut terminal, &mut app);
 
     disable_raw_mode().expect("could not disable terminal raw mode");
-    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture).unwrap();
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )
+    .unwrap();
     terminal.show_cursor().unwrap();
 }
 
-fn run_term(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> io::Result<()> {
+fn run_term(terminal: &mut Terminal<CrosstermBackend<Stdout>>, app: &mut App) -> io::Result<()> {
     loop {
-        terminal.draw(|f| ui(f))?;
+        terminal.draw(|f| ui(f, app))?;
 
         if let Event::Key(key) = event::read()? {
             if let KeyCode::Esc = key.code {
@@ -41,26 +53,37 @@ fn run_term(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> io::Result<()>
     }
 }
 
-fn ui(f: &mut Frame<CrosstermBackend<Stdout>>) {
-    let project: Rc<Project> = {
+fn ui(f: &mut Frame<CrosstermBackend<Stdout>>, app: &mut App) {
+    let (project, manifest): (Rc<Project>, Manifest) = {
         let cwd = std::env::current_dir().unwrap();
         let mut manifest_path = cwd.clone();
         manifest_path.push("assemblylift.toml");
 
         let asml_manifest =
             Manifest::read(&manifest_path).expect("could not read assemblylift.toml");
-        Rc::new(Project::new(asml_manifest.project.name.clone(), Some(cwd)))
+        (
+            Rc::new(Project::new(asml_manifest.project.name.clone(), Some(cwd))),
+            asml_manifest,
+        )
     };
+
+    let services_items: Vec<ListItem> = manifest
+        .services
+        .iter()
+        .map(|s| {
+            ListItem::new(&*s.name)
+        })
+        .collect_vec();
 
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints(
             [
                 Constraint::Max(3),
-                Constraint::Percentage(90),
+                Constraint::Percentage(85),
                 Constraint::Max(3),
             ]
-                .as_ref(),
+            .as_ref(),
         )
         .split(f.size());
 
@@ -71,13 +94,25 @@ fn ui(f: &mut Frame<CrosstermBackend<Stdout>>) {
         .alignment(Alignment::Left);
     f.render_widget(paragraph, chunks[0]);
 
-    let block = Block::default().title(format!("Project: {}", project.name.clone())).borders(Borders::ALL);
+    let block = Block::default()
+        .title(format!("Project: {}", project.name.clone()))
+        .borders(Borders::ALL);
     f.render_widget(block, chunks[1]);
 
-    let block = Block::default().borders(Borders::TOP|Borders::BOTTOM);
+    let block = Block::default().borders(Borders::TOP | Borders::BOTTOM);
     let paragraph = Paragraph::new("ESC to exit")
         .style(Style::default())
         .block(block)
         .alignment(Alignment::Left);
     f.render_widget(paragraph, chunks[2]);
+
+    let list = List::new(services_items)
+        .block(Block::default().borders(Borders::ALL).title("Services"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+    f.render_stateful_widget(list, chunks[1], &mut app.services.state);
 }
